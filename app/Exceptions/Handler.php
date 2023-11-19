06c546a77584b437af72c745cf04b60091f5fed6
Mayko Rodrigues <mak213k@yahoo.com.br>
2023-11-19 14:12:57 -0300

    project piloto de laravel api restfull com jwt

---
+++ app/Exceptions/Handler.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Exceptions;
+
+
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Throwable;
+
+class Handler extends ExceptionHandler
+{
+    /**
+     * A list of exception types with their corresponding custom log levels.
+     *
+     * @var array<class-string<\Throwable>, \Psr\Log\LogLevel::*>
+     */
+    protected $levels = [
+        //
+    ];
+
+    /**
+     * A list of the exception types that are not reported.
+     *
+     * @var array<int, class-string<\Throwable>>
+     */
+    protected $dontReport = [
+        //
+    ];
+
+    /**
+     * A list of the inputs that are never flashed to the session on validation exceptions.
+     *
+     * @var array<int, string>
+     */
+    protected $dontFlash = [
+        'current_password',
+        'password',
+        'password_confirmation',
+    ];
+
+    /**
+     * Register the exception handling callbacks for the application.
+     *
+     * @return void
+     */
+    public function render($request, Throwable $exception)
+    {
+        if($exception instanceof ValidationException){
+            return response()->json(
+                [
+                    'error' => $exception->errors(),
+                ]
+            )->setStatusCode(422);
+        }
+
+        if($exception instanceof MethodNotAllowedHttpException){
+
+            return response()->json([
+                'error' => $exception->getMessage(),
+            ])->setStatusCode(405);
+        }
+
+        return response()->json([
+            'error' => $exception->getMessage(),
+        ])->setStatusCode(500);
+    }
+}
